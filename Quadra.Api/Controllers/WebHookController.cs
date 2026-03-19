02e87136a1e9c02c46db00d8822c5f93c26a1d58
vinic <vinic@VINICIUDEV>
2026-03-19 16:54:35 -0300

    add end point webhook

---
+++ Quadra.Api/Controllers/WebHookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadra.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeebHookController : ControllerBase
+    {
+        [HttpPost("identificador")]
+        public async Task<IActionResult> Post([FromBody] string id)
+        {
+            return Ok("Conexão efetuada com sucesso");
+        }
+    }
+}
